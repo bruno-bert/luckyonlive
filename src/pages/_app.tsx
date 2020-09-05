af3a10b380e4c45c3ac50022d23e7b003d449fbc
Bruno de Paula <bruno.bert.jj@gmail.com>
2020-09-05 10:30:54 -0300

    added seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { SWRConfig } from 'swr';
+import { SWRConfig } from 'swr'
+import {NextSeo} from "next-seo"
 
 import ReactNotification, { store } from 'react-notifications-component'
 import 'react-notifications-component/dist/theme.css'
@@ -25,9 +26,25 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="Selecionar um usuário que comentar no chat de uma live no youtube"
+          content="Easily pick a random participant during youtube live transmission"
         />
       </Head>
+      <NextSeo
+        title="Lucky In Live - Pick a Random User During a Youtube Live Transmission"
+        description="Easily pick a random participant during youtube live transmission"
+        canonical="https://luckyinlive.vercel.app/"
+        openGraph={{
+          url: 'https://luckyinlive.vercel.app/',
+          title:
+            'Lucky In Live - Easily pick a random participant during youtube live transmission',
+          description:
+            'Easily pick a random participant during youtube live transmission',
+          images: [{ url: 'https://luckyinlive.vercel.app/img/cover.png' }],
+          site_name: 'Lucky In Libe',
+          locale: 'en_US'
+        }}
+      />
+
       <GlobalStyles />
       <Component {...pageProps} />
       </SWRConfig>
